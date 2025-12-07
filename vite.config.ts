c2ea608dfb93c52a0b11fd4b86613685556f0e9c
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2025-12-07 18:34:44 +0530

    gbg

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nutriomfit-holistic-health",
+  base: "/nutriomfit-harmony-hub/",
   server: {
     host: "::",
     port: 8080,
